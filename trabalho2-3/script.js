39dd18e15a2c6b2bba93cf2e01b81753027c7181
Levye-Fm <levi.ferreira08@aluno.ifce.edu.br>
2025-01-01 21:21:31 -0300

    Add files via upload

---
+++ trabalho2-3/script.js
@@ -0,0 +1,14 @@
+function pereca(x) {
+    var mesaInfo = document.getElementById('mesaInfo'+x);
+
+    mesaInfo.style.display = 'block';
+    mesaInfo.style.top = '100px';
+    
+}
+
+function closes(x) {
+    var mesaInfo = document.getElementById('mesaInfo'+x);
+
+
+    mesaInfo.style.display = 'none';
+}
