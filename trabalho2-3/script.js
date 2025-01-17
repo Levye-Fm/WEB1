0a451be5064ed7ff0de200a0361a191ee00409f3
Levye-Fm <levi.ferreira08@aluno.ifce.edu.br>
2025-01-16 21:55:31 -0300

    Add files via upload

--- trabalho2-3/script.js
+++ trabalho2-3/script.js
@@ -12,3 +12,26 @@ function closes(x) {
 
     mesaInfo.style.display = 'none';
 }
+
+function modar(x) {
+    var mesa = document.getElementById('mesa'+x);
+    var mosa = document.getElementById('mosa'+x);
+    var md = document.getElementById('md'+x);
+
+    if (mosa.innerHTML == "disponivel") {
+        mosa.innerHTML = "reservada";
+        md.innerHTML = "reservada";
+        mesa.style.backgroundColor = '#ffba3b';   
+    }
+    else if (mosa.innerHTML == 'reservada') {
+        mosa.innerHTML = "em uso";
+        md.innerHTML = "em uso";
+        mesa.style.backgroundColor = '#F44336';
+    }
+    else {
+        mosa.innerHTML = "disponivel";
+        md.innerHTML = "disponivel";
+        mesa.style.backgroundColor = '#0084ff ';
+    }
+
+}
